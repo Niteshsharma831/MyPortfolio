7790a82df74dfd768e8e35cc81e27c52e4292ae7
NItesh Kumar Sharma <niteshkumarsharma831@gmail.com>
2025-06-24 19:26:15 +0530

    upadte

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or better: origin: 'https://your-frontend-url.com'
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
