e529a16ffef3332e44d19a14a109801bc3c8689c
NItesh Kumar Sharma <niteshkumarsharma831@gmail.com>
2025-07-30 00:29:46 +0530

    Remove static file serving and catch-all route from server.js

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Portfolio Backend");
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
 app.listen(port, () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
 });
